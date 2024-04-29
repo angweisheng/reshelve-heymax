38622560337af9d7811a0463d7224e99fb3eeac6
Wei Sheng Ang <aws1@hotmail.sg>
2024-04-29 21:57:07 +0300

    book listing page

---
+++ src/app/books/page.tsx
@@ -0,0 +1,16 @@
+import BookReel from "@/components/BookReel"
+import MaxWidthWrapper from "@/components/MaxWidthWrapper"
+
+const Page = () => {
+    return (
+        <MaxWidthWrapper>
+        <div>
+            <BookReel title="Listed Books" />
+        </div>
+        </MaxWidthWrapper>
+
+            
+        )
+} 
+
+export default Page
---
+++ src/components/BookReel.tsx
@@ -0,0 +1,27 @@
+
+interface BookReelProps {
+  title: string;
+  subtitle?: string;
+}
+
+const BookReel = (props: BookReelProps) => {
+  const { title, subtitle } = props;
+  return (
+    <section className="py-12">
+      <div className="md:flex md:items-center md:justify-between mb-4">
+        <div className="max-w-2x1 px4 lg:max-w-4xl lg:px-0">
+          {title ? (
+            <h1 className="text-2xl font-bold text-gray-900 sm:text-3xl">
+              {title}
+            </h1>
+          ) : null}
+          {subtitle ? (
+            <p className="mt-2 text-sm text-muted-foreground">{subtitle}</p>
+          ) : null}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default BookReel;
